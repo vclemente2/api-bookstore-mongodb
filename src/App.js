b70fb4f534d210969f0f410a13f6999bdf3a8a20
Vinicius Bastos <vclemente2@hotmail.com>
2023-06-10 21:43:33 -0300

    Configuração do servidor.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import express from "express";
 
+class App {
+  constructor() {
+    this.app = express();
+
+    this.app.use(express.json());
+    this.app.get("/", (_, res) => {
+      return res.json({
+        version: "1.0.0",
+        description: "Bookstore API",
+        author: "Vinicius Bastos Clemente"
+      });
+    });
+  }
+
+  getApp() {
+    return this.app;
+  }
+}
+
+export default App;
--- src/Server.js
+++ src/Server.js
@@ -1,1 +1,15 @@
+import "dotenv/config.js";
+import App from "./App.js";
 
+class Server {
+  constructor() {
+    this.app = new App().getApp();
+    this.port = process.env.PORT || 3000;
+
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on http://localhost:${this.port}`);
+    });
+  }
+}
+
+export default new Server();
